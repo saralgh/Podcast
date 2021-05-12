efdb76fefa43ae5c88aa5a74cbb98c65b8c86dae
Sara Ramírez Chávez <46709918+saralgh@users.noreply.github.com>
2021-05-12 18:18:23 +0200

    Add files via upload

---
+++ migrations/Version20210510120315.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210510120315 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE podcast (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, tittle VARCHAR(255) NOT NULL, fecha_creacion DATE NOT NULL, description LONGTEXT NOT NULL, audio VARCHAR(255) NOT NULL, imagen VARCHAR(255) NOT NULL, INDEX IDX_D7E805BD9D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, type_user VARCHAR(255) NOT NULL, nombre VARCHAR(255) NOT NULL, apellidos VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE podcast ADD CONSTRAINT FK_D7E805BD9D86650F FOREIGN KEY (user_id_id) REFERENCES `user` (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE podcast DROP FOREIGN KEY FK_D7E805BD9D86650F');
+        $this->addSql('DROP TABLE podcast');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
